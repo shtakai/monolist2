3c8823274a501349255cd70e77b14c0c4809943d
Alyson T <shtakai@gmail.com>
2016-11-16 02:00:48 +0000

    added relationship

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ActiveRecord::Base
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+  
+  # STI for items
+  # to grab (want/have) users, use (want/have)_users
+  has_many :wants, class_name: "Want", foreign_key: "item_id", dependent: :destroy
+  has_many :want_users, through: :wants, source: :user
+  has_many :haves, class_name: "Have", foreign_key: "item_id", dependent: :destroy
+  has_many :have_users, through: :haves, source: :user
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,13 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
+  
+  # STI for items
+  # to grab (want/have) items, use (want/have)_items
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items, through: :wants, source: :item
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items, through: :haves, source: :item
 
 
   # 他のユーザーをフォローする
