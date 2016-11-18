164ea00b63e06af6775d08a9353b7d590edefd80
Alyson T <shtakai@gmail.com>
2016-11-18 06:13:45 +0000

    grab owned ranking

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -1,4 +1,17 @@
 class Ownership < ActiveRecord::Base
   belongs_to :user, class_name: "User"
   belongs_to :item, class_name: "Item"
+  
+  LIMIT = 50
+  
+  # TODO: offset / page (kaminari)
+  def self.ranking_owned(own_type: nil)
+    where(type: own_type).
+        group(:item_id).
+        order('count_all desc').
+        limit(LIMIT).
+        count.
+        map{ |id, count| {id: id, count: count} }
+  end
+  
 end
