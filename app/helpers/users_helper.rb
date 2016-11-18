2679cc5df7f1c2a28e7a6bc75cf11ffa2e92f468
Alyson T <shtakai@gmail.com>
2016-11-18 04:35:45 +0000

    modified for link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,12 @@
 module UsersHelper
-  def gravatar_for(user, options = { size: 80 })
+  def gravatar_for(user, options = { size: 80, link: false })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    if options[:link]
+      link_to image_tag(gravatar_url, alt: user.name, class: "gravatar"), user_path(user)
+    else
+      image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
   end
 end
