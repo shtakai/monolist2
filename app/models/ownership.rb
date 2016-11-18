6b24ff1bfc65001000b6ebebf81ba9eccfd17b89
Alyson T <shtakai@gmail.com>
2016-11-18 10:32:58 +0000

    added method for ranking

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -11,7 +11,7 @@ class Ownership < ActiveRecord::Base
         order('count_all desc').
         limit(RANKING_LIMIT).
         count.
-        map{ |id, count| {item: Item.find(id), count: count} }
+        map.with_index(1) { |elem, index| {index: index, item: Item.find(elem[0]), count: elem[1]} }
         # map{ |id, count| {item: Item.find(id), count: count} }
         # NOTE: ↑のmapは最適化できるのか？
   end
